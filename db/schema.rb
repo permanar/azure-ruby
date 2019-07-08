57a959a1bb752570a3abda85311ad2c95d3eb68e
Richie Permana <richie.permana@gmail.com>
2019-07-08 14:44:50 +0800

    commits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_25_070230) do
+ActiveRecord::Schema.define(version: 2019_07_08_060952) do
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "caption"
